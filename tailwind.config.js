f5f2efbe723078592ad0ae7db794ca36d9650485
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-03-03 22:57:24 +0100

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,25 @@ export default {
   darkMode: "false",
   theme: {
     extend: {
-      screens: {         
-        'xxl': '1440px',       
-                },     
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "0.875rem",
+          sm: "1.125rem",
+          md: "1.125rem",
+          lg: "0.625rem",
+          xl: "0.625rem",
+          "2xl": "0.625rem",
+        },
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1580px",
+          
+        },
+      },
     },
   },
   plugins: [require("flowbite/plugin"), require("tailwindcss-animated")],
