e57ca999fad24c35542646ea944432368167f422
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-03-15 21:49:27 +0100

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  // ...
-  integrations: [tailwind()],
+  // Enable many frameworks to support all different kinds of components.
+  // No `include` is needed if you are only using a single JSX framework!
+  integrations: [tailwind(), react()]
 });
---
+++ src/types/window.d.ts
@@ -0,0 +1,6 @@
+export interface particlesInit extends Window {
+  customAttribute: any;
+}
+export interface particlesLoaded extends Window {
+  customAttribute: any;
+}
