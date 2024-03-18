15084b8f2ce4e8546f27a72f87ed29399b35708c
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-03-18 17:33:44 +0100

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable many frameworks to support all different kinds of components.
   // No `include` is needed if you are only using a single JSX framework!
-  integrations: [tailwind(), react()]
+  site: 'https://snazzy-capybara-a7228d.netlify.app/',
+  integrations: [
+    tailwind(),
+    react(), 
+    partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    }}),
+  ],
 });
