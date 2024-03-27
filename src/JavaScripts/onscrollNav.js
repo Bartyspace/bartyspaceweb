943ffb613c057f52e43bf0a20145a11e74e40c0c
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-03-27 19:48:40 +0100

    initial

--- src/JavaScripts/onscrollNav.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 50) {
-       $('#dynamic').addClass('newClass');
-    } else {
-       $('#dynamic').removeClass('newClass');
-    }
-});
