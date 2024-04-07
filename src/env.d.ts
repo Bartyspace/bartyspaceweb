e6d3461b1cf34f3d57a1945affa38e7db9c9c8f8
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-04-07 19:49:09 +0200

    i

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+type NetlifyLocals = import('@astrojs/netlify').NetlifyLocals
+
+declare namespace App {
+  interface Locals extends NetlifyLocals {
+    // ...
+  }
+}
