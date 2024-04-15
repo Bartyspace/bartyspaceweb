1cfb3a05d3eaa0343260dbdc47d75b498351e972
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-04-15 21:29:34 +0200

    i

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,29 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import partytown from "@astrojs/partytown";
-import svelte from "@astrojs/svelte";
-import sitemap from "@astrojs/sitemap";
+import {defineConfig} from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
+import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
+import lighthouse from 'astro-lighthouse';
 
 // https://astro.build/config
 export default defineConfig({
-
   site: 'https://bartyspacelabs.netlify.app/',
 
   integrations: [
-    tailwind(), 
-    react(), 
+    tailwind(),
+    react(),
     partytown({
       config: {
-        forward: ["dataLayer.push"]
+        forward: ['dataLayer.push']
       }
-    }), 
-    svelte(), 
+    }),
+    svelte(),
     sitemap({
       changefreq: 'weekly',
       priority: 0.7,
-      lastmod: new Date('2022-02-24'),
-    })],
+      lastmod: new Date('2022-02-24')
+    }),
+    lighthouse()
+  ]
 });
---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+const siteConfig = {
+  name: 'Bartyspace Labs',
+  slogan: 'Společně vytváříme nové standardy pro Webový design a Vývoj stránek',
+  description:
+    'Objevte webové inovace na Bartyspace Labs - váš digitální portál pro špičkový vývoj a design. Navštivte nás na bartyspacelabs.netlify.app.',
+  email: 'bartys2000@gmail.com',
+  url: 'https://bartyspacelabs.netlify.app',
+  links: {
+    sourcecode: 'https://github.com/Bartyspace/bartyspaceweb',
+    github: 'https://https://github.com/Bartyspacem',
+  }
+} as const;
+
+export default siteConfig;
