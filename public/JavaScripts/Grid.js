75f24dd7d478851f5d3be7fc4675f76c098c381d
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-04-21 20:03:57 +0200

    i

---
+++ public/JavaScripts/Grid.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Check if screen width is greater than 1024px
+  if (window.innerWidth > 1024) {
+    const gridMoveDiv = document.getElementById('gridmove');
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const divWidth = gridMoveDiv.offsetWidth;
+    const divHeight = gridMoveDiv.offsetHeight;
+
+    const horizontalOffset = (screenWidth - divWidth) / 10;
+    const verticalOffset = (screenHeight - divHeight) / 10;
+
+    gridMoveDiv.style.transform = `translate(${horizontalOffset}px, ${verticalOffset}px)`;
+
+    document.addEventListener('mousemove', event => {
+      const xOffset = (event.clientX - screenWidth / 2) / screenWidth;
+      // Set yOffset to 0 to prevent vertical movement
+      const yOffset = 0;
+      gridMoveDiv.style.transform = `translate(${horizontalOffset + xOffset * 20}px, ${verticalOffset + yOffset * 20}px)`;
+    });
+  }
+});
