dcc5111ea8c0ae03454291f07b1de76f936a2a84
Bartyspace <160775685+Bartyspace@users.noreply.github.com>
2024-05-06 11:14:04 +0200

    i

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,23 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bartyspacelabs.netlify.app/',
   trailingSlash: 'never',
-  integrations: [
-    tailwind({
-      nesting: true
-    }),
-    partytown({
-      config: {
-        forward: ['dataLayer.push']
-      }
-    }),
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date('2022-02-24')
-    }),
-  ]
+  integrations: [tailwind({
+    nesting: true
+  }), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), sitemap({
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date('2022-02-24')
+  }), icon()]
 });
