603174fb0ccf0d160940430158cc15f9f16de4f0
Bartys <160775685+Bartyspace@users.noreply.github.com>
2025-09-09 19:43:05 +0000

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bartyspacelabs.tech/',
+  site: 'https://bartyspacelabs.netlify.app/',
   trailingSlash: 'never',
 
   integrations: [
